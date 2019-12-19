f42fa1b5a9573afbd2713cb5b92efa584fc161c4
Enrique Franco <luisfrancoz1@gmail.com>
2019-12-19 17:33:24 -0500

    Ref Updating toggleDone to Use the API

--- app/javascript/packs/app.js
+++ app/javascript/packs/app.js
@@ -62,8 +62,13 @@ document.addEventListener("DOMContentLoaded", () => {
         let task = this.tasks.find(item => item.id == id);
         if (task) {
           task.completed = !task.completed;
-          console.log('task toggled')
-          this.message = `Task ${id} has been completed`
+          this.task = task;
+          Api.updateTask(this.task).then(function(response){
+            app.listTasks();
+            app.clear();
+            let status = response.completed ? 'completed' : 'in progress'
+            app.message = `Task ${response.id} is ${status}.`
+          })
         }
       },
       createTask: function(event){
